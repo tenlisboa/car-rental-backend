62b856037a608245f2aaff7cfb87c7c4d2fcf2db
Gabriel Lisboa <gabrielbento.rx@outlook.com>
2022-09-06 21:13:15 -0300

    ref: Specification from memory to db

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,6 +1,8 @@
 import { DataSource } from "typeorm";
 import { Category } from "../modules/cars/entities/Category";
+import { Specification } from "../modules/cars/entities/Specification";
 import { CreateCategories1662203203498 } from "./migrations/1662203203498-CreateCategories";
+import { CreateSpecifications1662509109492 } from "./migrations/1662509109492-CreateSpecifications";
 
 const AppDataSource = new DataSource({
   type: (process.env.DB_DRIVER as "postgres") || "postgres",
@@ -9,8 +11,11 @@ const AppDataSource = new DataSource({
   username: process.env.DB_USERNAME || "sample",
   password: process.env.DB_PASSWORD || "longpassword",
   database: process.env.DB_DATABASE || "rental",
-  entities: [Category],
-  migrations: [CreateCategories1662203203498],
+  entities: [Category, Specification],
+  migrations: [
+    CreateCategories1662203203498,
+    CreateSpecifications1662509109492,
+  ],
 });
 
 export { AppDataSource };
--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -1,3 +1,5 @@
+import { Repository } from "typeorm";
+import { AppDataSource } from "../../../../database";
 import { Specification } from "../../entities/Specification";
 import {
   ICreateSpecificationDTO,
@@ -5,37 +7,25 @@ import {
 } from "../ISpecificationsRepository";
 
 class SpecificationsRepository implements ISpecificationsRepository {
-  private specifications: Specification[];
-  private static INSTANCE: SpecificationsRepository;
+  private repository: Repository<Specification>;
 
   constructor() {
-    this.specifications = [];
-  }
-
-  public static getInstance(): SpecificationsRepository {
-    if (!SpecificationsRepository.INSTANCE) {
-      SpecificationsRepository.INSTANCE = new SpecificationsRepository();
-    }
-
-    return SpecificationsRepository.INSTANCE;
+    this.repository = AppDataSource.getRepository(Specification);
   }
 
   async findByName(name: string): Promise<Specification> {
-    const specification = this.specifications.find((s) => s.name === name);
+    const specification = await this.repository.findOneBy({ name });
 
     return specification!;
   }
 
   async create({ name, description }: ICreateSpecificationDTO): Promise<void> {
-    const specification = new Specification();
-
-    Object.assign(specification, {
+    const specification = this.repository.create({
       name,
       description,
-      created_at: new Date(),
     });
 
-    this.specifications.push(specification);
+    await this.repository.save(specification);
   }
 }
 
